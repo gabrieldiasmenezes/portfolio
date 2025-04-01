cd241522f1021e8b1b5b4e583b093b5514cc48e9
gabrieldiasmenezes <g.dias120306@gmail.com>
2025-04-01 11:43:50 -0300

    adicionando projetos

---
+++ src/routes/Projects/ProjectContent.ts
@@ -0,0 +1,149 @@
+export const textProjects = {
+    pt: {
+        title: "Explore Meu Universo de Projetos!",
+        subTitle: "Descubra soluções inovadoras, aplicações criativas e minha jornada no desenvolvimento de software.",
+        projects: [
+            {
+                title: "Sovrano API 🍽️",
+                description: "Sovrano API é o back-end do sistema de reservas online sofisticado. Ele gerencia de forma segura e eficiente todas as interações de reserva, com autenticação segura e controle completo das reservas no restaurante.",
+                link: "https://github.com/gabrieldiasmenezes/reserva-Sovrano",
+                image: "sovranoImgAPI.jpg"
+            },
+            {
+                title: "Sovrano Web 🍽️",
+                description: "Sovrano Web é o front-end do sistema de reservas online sofisticado. Ele oferece uma interface intuitiva e elegante para clientes de restaurantes de alto padrão, com um processo de reserva simples e eficiente.",
+                link: "https://github.com/gabrieldiasmenezes/sovrano-web",
+                image: "sovranoImg.jpg"
+            },               
+            {
+                title: "FinNet 🚀",
+                description: "FinNet é um sistema de gerenciamento de contas bancárias desenvolvido em Java e Spring Boot. Com validação inteligente e banco de dados em memória, oferece controle eficiente e seguro para contas pessoais e empresariais.",
+                link: "https://github.com/gabrieldiasmenezes/FinNet",
+                image: "finnet.png"
+            },
+            {
+                title: "Money Control Web 💻",
+                description: "O Money Control é uma plataforma completa para gestão financeira pessoal e empresarial. O Money Control Web funciona como o front-end do projeto, oferecendo uma interface moderna e intuitiva, construída com Next.js, shadcn/ui e Lucide, para proporcionar uma experiência de usuário incrível.",
+                link: "https://github.com/gabrieldiasmenezes/money-control-web",
+                image: "moneyControl.png"
+            },
+            {
+                title: "Money Control API 💸",
+                description: "O Money Control API é o back-end do projeto, fornecendo uma API RESTful segura e eficiente para gerenciamento financeiro. Ele suporta contas bancárias, transações, orçamentos e relatórios, garantindo controle total das finanças.",
+                link: "https://github.com/gabrieldiasmenezes/money-control-api",
+                image: "moneyControl.png"
+            },
+            {
+                title: "Box-Pokemon",
+                description: "Pokémon Box é um projeto em Python que permite organizar e acompanhar capturas de Pokémon, registrando detalhes e monitorando o progresso na Pokédex de forma intuitiva.",
+                link: "https://github.com/gabrieldiasmenezes/Box-Pokemon",
+                image: "pkm.jpg"
+            },
+            {
+                title: "Pet-Service",
+                description: "Projeto desenvolvido em React para cadastrar e gerenciar pets, permitindo que tutores acompanhem informações, histórico e agendem serviços como pet shop e veterinário de forma prática e organizada.",
+                link: "https://github.com/gabrieldiasmenezes/PetService",
+                image: "petS.png"
+            },
+            {
+                title: "Solar Flower",
+                description: "Solar Flower é um projeto desenvolvido em React com o objetivo de promover o uso de energia sustentável e renovável por meio da tecnologia. Inspirado no Smart Flower, essa solução foi criada para otimizar a captação e o aproveitamento de energia solar de forma eficiente e inovadora.",
+                link: "https://github.com/gabrieldiasmenezes/solarflower",
+                image: "solarFlower.png"
+            },
+            {
+                title: "Help Car",
+                description: "HelpCar é um projeto desenvolvido em Next.js para auxiliar motoristas com problemas mecânicos do dia a dia. Através de um chatbot interativo, os usuários podem realizar autodiagnósticos, obter autoorçamentos e até agendar serviços em mecânicas, otimizando tempo e proporcionando mais praticidade no cuidado com seus veículos.",
+                link: "https://github.com/gabrieldiasmenezes/helpcar",
+                image: "helpCar.png"
+            },
+            {
+                title: "Nautilus-Monitoring",
+                description: "Nautilus-Monitoring é um projeto desenvolvido em JavaScript, HTML e CSS com o objetivo de conscientizar sobre a preservação dos habitats marinhos. A plataforma fornece informações sobre a importância desses ecossistemas para a vida marinha e os impactos negativos do lixo nos ambientes marinhos, destacando a necessidade de ações para proteger esses locais vitais para o equilíbrio ambiental.",
+                link: "https://github.com/gabrieldiasmenezes/Nautilus-Monitoring",
+                image: "NM.png"
+            },
+            {
+                title: "Vila Sapê",
+                description: "Projeto Vila Sapê é um site desenvolvido em HTML e CSS, inspirado na pousada Vila Sapê. A plataforma apresenta os serviços e a qualidade da pousada, além de possibilitar a realização de reservas de forma prática e intuitiva, proporcionando uma experiência completa aos visitantes interessados em desfrutar do local.",
+                link: "https://github.com/gabrieldiasmenezes/site-pousada",
+                image: "pousada.png"
+            }
+            
+        ]
+    },
+    en: {
+        title: "Explore My Universe of Projects!",
+        subTitle: "Discover innovative solutions, creative applications, and my journey in software development.",
+        projects: [
+            {
+                title: "Sovrano API 🍽️",
+                description: "Sovrano API is the back-end of the sophisticated online reservation system. It securely and efficiently manages all reservation interactions, with secure authentication and full control over restaurant bookings.",
+                link: "https://github.com/gabrieldiasmenezes/reserva-Sovrano",
+                image: "sovranoImgAPI.jpg"
+            },
+            {
+                title: "Sovrano Web 🍽️",
+                description: "Sovrano Web is the front-end of the sophisticated online reservation system. It provides an intuitive and elegant interface for high-end restaurant clients, ensuring a seamless and efficient reservation process.",
+                link: "https://github.com/gabrieldiasmenezes/sovrano-web",
+                image: "sovranoImg.jpg"
+            },              
+            {
+                title: "FinNet 🚀",
+                description: "FinNet is a banking account management system built with Java and Spring Boot. It features smart validation and in-memory database support for secure and efficient financial control.",
+                link: "https://github.com/gabrieldiasmenezes/FinNet",
+                image: "finnet.png"
+            },  
+            {
+                title: "Money Control Web 💻",
+                description: "Money Control is a complete financial management platform for personal and business use. Money Control Web serves as the front-end of the project, offering a modern and intuitive interface built with Next.js, shadcn/ui, and Lucide to provide an amazing user experience.",
+                link: "https://github.com/gabrieldiasmenezes/money-control-web",
+                image: "moneyControl.png"
+            },
+            {
+                title: "Money Control API 💸",
+                description: "Money Control API is the back-end of the project, providing a secure and efficient RESTful API for financial management. It supports bank accounts, transactions, budgets, and reports, ensuring full financial control.",
+                link: "https://github.com/gabrieldiasmenezes/money-control-api",
+                image: "moneyControl.png"
+            },                             
+            {
+                title: "Box-Pokemon",
+                description: "Pokémon Box is a Python project that allows users to organize and track Pokémon captures, recording details and monitoring progress in the Pokédex intuitively.",
+                link: "https://github.com/gabrieldiasmenezes/Box-Pokemon",
+                image: "pkm.jpg"
+            },
+            {
+                title: "Pet-Service",
+                description: "A React project designed to register and manage pets, enabling owners to track information, history, and schedule services such as pet grooming and veterinary appointments conveniently.",
+                link: "https://github.com/gabrieldiasmenezes/PetService",
+                image: "petS.png"
+            },
+            {
+                title: "Solar Flower",
+                description: "Solar Flower is a React project aimed at promoting the use of sustainable and renewable energy through technology. Inspired by Smart Flower, this solution was created to optimize the capture and utilization of solar energy efficiently and innovatively.",
+                link: "https://github.com/gabrieldiasmenezes/solarflower",
+                image: "solarFlower.png"
+            },
+            {
+                title: "Help Car",
+                description: "HelpCar is a Next.js project designed to assist drivers with everyday mechanical issues. Through an interactive chatbot, users can perform self-diagnoses, obtain instant cost estimates, and even schedule services at repair shops, optimizing time and convenience in vehicle maintenance.",
+                link: "https://github.com/gabrieldiasmenezes/helpcar",
+                image: "helpCar.png"
+            },
+            {
+                title: "Nautilus-Monitoring",
+                description: "Nautilus-Monitoring is a project developed using JavaScript, HTML, and CSS with the goal of raising awareness about the preservation of marine habitats. The platform provides information on the importance of these ecosystems for marine life and the negative impacts of waste on marine environments, highlighting the need for actions to protect these vital areas for environmental balance.",
+                link: "https://github.com/gabrieldiasmenezes/Nautilus-Monitoring",
+                image: "NM.png"
+            },
+            {
+                title: "Vila Sapê",
+                description: "Vila Sapê Project is a website developed using HTML and CSS, inspired by the Vila Sapê inn. The platform showcases the services and quality of the inn, as well as enabling practical and intuitive reservations, providing a complete experience for visitors interested in enjoying the location.",
+                link: "https://github.com/gabrieldiasmenezes/site-pousada",
+                image: "pousada.png"
+            }
+            
+        ]
+    }
+};
+
